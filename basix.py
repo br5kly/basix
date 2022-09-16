0afb7f246bccc7e3207e6365cea0ee3fc8b7399e
br5kly <108040523+br5kly@users.noreply.github.com>
2022-09-16 20:37:17 +0200

    Create basix.py

---
+++ basix.py
@@ -0,0 +1,24 @@
+import os
+try:
+    import rich
+except ImportError:
+    os.system('pip install rich')
+try:
+    import requests
+except ImportError:
+    os.system('pip install reqeusts')
+try:
+    import bs4
+except ImportError:
+    os.system('pip install bs4')
+try:
+    import stodiomask
+except ImportError:
+    os.system('pip install stodiomask')
+
+try:
+    import reqeusts
+    print("1000000000% DOOOONE \n BY ZEYAD ALABANY")
+except ImportError:
+    print("\033[1;31m ERROR MYABE YOUR TERMUX CRASH DOWNLOAD ORGINALA TERMUX FROM HERE")
+    os.system('xdg-open https://f-droid.org/repo/com.termux_118.apk")
